a3183c717de0e72c5f806bd91b5363d9faaecfde
Bahri-Haythem <haythem.bahri8@hotmail.com>
2020-03-19 15:25:06 +0100

    today's first commit

--- src/app/games/games.service.ts
+++ src/app/games/games.service.ts
@@ -1,27 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 const httpOptions = {
-	headers: new HttpHeaders({ "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-	"x-rapidapi-key": "b4ffabe329msh6a4570ddd8934eap1fd831jsne57777e93e67" })
+  headers: new HttpHeaders({
+    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+    "x-rapidapi-key": "b4ffabe329msh6a4570ddd8934eap1fd831jsne57777e93e67"
+  })
 };
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GamesService {
-data1 : any;
+  data1: any;
 
+  getGames(i: any): any {
+    let j = 2;
+    let URL = `http://localhost:5000/api/Games/${i}`;
 
-getGames(i: any) :any{
+    this.data1 = this.http.get(URL, httpOptions);
 
+    return this.data1;
+  }
 
-	 let j = 2;
-	 let URL  =`https://rawg-video-games-database.p.rapidapi.com/games?page=${i} `;
-	
-	this.data1 =  this.http.get(URL,httpOptions) ;
-	
-	
-	 return this.data1;
-}
-  
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {}
 }
