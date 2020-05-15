87a53e2d660a0ec515a0974446edb59adeba6105
Bahri-Haythem <haythem.bahri8@hotmail.com>
2020-05-15 19:26:03 +0100

    haythem changes --last

--- PauseProject_Frontend/src/app/pages/examples/registerpage/registerpage.component.ts
+++ PauseProject_Frontend/src/app/pages/examples/registerpage/registerpage.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationService } from "src/app/authentication.service";
 @Component({
   selector: "app-registerpage",
   templateUrl: "registerpage.component.html",
+  styleUrls: ["registerpage.component.scss"],
 })
 export class RegisterpageComponent implements OnInit, OnDestroy {
   isCollapsed = true;
