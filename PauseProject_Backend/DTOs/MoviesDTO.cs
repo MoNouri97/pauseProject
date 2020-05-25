cb399f6bbcaf334d5d0ac42423787cf46db5eecf
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-05-25 10:44:11 +0100

    changed movies backend ( small change)

--- PauseProject_Backend/DTOs/MoviesDTO.cs
+++ PauseProject_Backend/DTOs/MoviesDTO.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace PauseProject.DTOs
 {
-    public class MoviesDTO
-    {
-        [JsonProperty("id")]
-        public int id { get; set; }
+	public class MoviesDTO
+	{
+		[JsonProperty("id")]
+		public int id { get; set; }
 
-        [JsonProperty("backdrop_path")]
-        public string img { get; set; }
-        [JsonProperty("release_date")]
-        public string release_date { get; set; }
-        [JsonProperty("title")]
-        public string title { get; set; }
+		[JsonProperty("poster_path")]
+		public string img { get; set; }
+		[JsonProperty("release_date")]
+		public string release_date { get; set; }
+		[JsonProperty("title")]
+		public string title { get; set; }
 
-       
-    }
+
+	}
 }
